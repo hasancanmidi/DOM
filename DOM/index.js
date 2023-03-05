b9d4de3bf19928964caf0cbd56d9b042624a6df4
hasancanmidi <hasancanmidi@hotmail.com>
2023-03-05 21:24:27 +0300

    update

--- DOM/index.js
+++ DOM/index.js
@@ -32,6 +32,7 @@ document.getElementById("title").innerHTML = "Good BY";
 
 document.querySelector("h1");
 //getElementById gibi yalnızca tek öğe döndürür.
+
 document.querySelector("#title");
 //İd Class isim seçebiliyoruz.
 document.querySelector(".btn");
@@ -40,17 +41,39 @@ document.querySelector("li.item");
 
 document.querySelector("#list .item");
 // sadece 1. itemi alır hepsini almamız için
+
 document.querySelectorAll("#list .item");
 //kullanmamız gerekir.
+
 document.querySelectorAll("#list .item")[2].style.color="red";
 //Değişiklikte bu şekilde yapılır.
+
 document.querySelector(".btn").style.backgroundColor = "Yellow";
 //Button class'ını seçerek arka planlarını sarı yaptık.
+
 document.querySelector("button").classList
 //sınıfların listesini verir
+
 document.querySelector("button").classList.add("invisible");
 //Button sınıfına invisible css'ini ekledik.
+
 document.querySelector("button").classList.remove("invisible");
 //Button sınıfına invisible css'ini sildik.
+
 document.querySelector("button").classList.toggle("invisible");
-//çalıştır kapat
+//aç kapat
+
+document.querySelector("h1").classList.add("huge");
+// Huge diye bir class oluşturduk css'de sonrada h1'e onu uyguladık
+
+document.querySelector("title").textContent;
+//sadece metin içeriğini verir (strong italic gbi özellikleri göstermez)
+
+document.querySelector("a").attributes;
+// Attributlerimizi gösterir.
+
+document.querySelector("a").getAttribute("href");
+// Attribute'mizi görürüz
+
+document.querySelector("a").setAttribute("href","https://www.bing.com");
+// Attribute'mizi değiştiririz
