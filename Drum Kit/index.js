8433e25befa867ca2aeb4e8db13734ca4b3c397a
hasancanmidi <hasancanmidi@hotmail.com>
2023-03-07 18:38:16 +0300

    update

---
+++ Drum Kit/index.js
@@ -0,0 +1,9 @@
+// addEventListener() olay  tetiklendikten sonra çağrılan işlevdir. 
+
+var numberOfDrumButtons = document.querySelectorAll(".drum").length;
+
+for (var i = 0; i < numberOfDrumButtons; i++) {
+    document.querySelectorAll(".drum")[i].addEventListener("click", function () {
+        alert("I got click")
+    });
+}
